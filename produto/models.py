c9236cb81e71b1c1bf3e66a7329d8604085343c6
GaBatista <gabrielbatista95@hotmail.com>
2021-01-21 12:45:21 -0300

    listando e detalhando produtos

--- produto/models.py
+++ produto/models.py
@@ -2,19 +2,31 @@ from django.db import models
 from PIL import Image
 import os
 from django.conf import settings
+from django.utils.text import slugify
+from utils import utils
+
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
     descricao_curta = models.TextField(max_length=255)
     descricao_longa = models.TextField()
     imagem = models.ImageField(upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     preco_marketing = models.FloatField()
     preco_marketing_promocional = models.FloatField(default=0)
-    tipo = models.CharField(default='V',max_length=1,choices=(('V', 'Variação'),('S', 'Simples'),))
+    tipo = models.CharField(default='V',max_length=1,choices=(('V', 'Variável'),('S', 'Simples'),))
     
     def __str__(self):
         return self.nome
+
+    def get_preco_formatado(self):
+        return utils.formata_preco(self.preco_marketing)
+    get_preco_formatado.short_description = 'Preço'
+
+    def get_preco_promocional_formatado(self):
+        return utils.formata_preco(self.preco_marketing_promocional)
+    get_preco_promocional_formatado.short_description = 'Preço Promocional'
+
 
     @staticmethod
     def resize_image(img, new_width=800):
@@ -36,6 +48,10 @@ class Produto(models.Model):
 
 
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}-{self.pk}'
+            self.slug = slug
+        
         super().save(*args, **kwargs)
 
         max_image_size = 800
